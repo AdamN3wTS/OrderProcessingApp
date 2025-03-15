73173222314b7c501ca5f4697d312a95afa87d2f
AdamN3wTS <adamjanus2004@gmail.com>
2025-03-15 11:19:43 +0100

    Wprowadzam Logike Biznesową w OrderService oraz w Repositories dodaje klase InMemoryOrderRepositories która służy jako mechanizm przechowywania zamówien i jakies proste operacje oraz dzięki temu odzielam obowiązki. Dzieki temu będzie mi łatwiej zrobić testy

--- OrderProcessingApp/Domain/Services/OrderService.cs
+++ OrderProcessingApp/Domain/Services/OrderService.cs
@@ -3,10 +3,95 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OrderProcessingApp.Domain.Entities;
+using OrderProcessingApp.Domain.Enums;
+using OrderProcessingApp.Repositories;
 
 namespace OrderProcessingApp.Domain.Services
 {
-    internal class OrderService
+    public class OrderService
     {
+        private readonly InMemoryOrderRepository _repository;
+
+        public OrderService(InMemoryOrderRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddOrder(Order order)
+        {
+
+            _repository.AddOrder(order);
+        }
+
+        public void RemoveOrder(int orderId)
+        {
+
+            _repository.RemoveOrderById(orderId);
+        }
+
+        public void SendToWarehouse(int orderId)
+        {
+
+            var order = _repository.GetOrderById(orderId);
+            if (order == null)
+            {
+                Console.WriteLine($"Zamówienie o ID {orderId} nie istnieje.");
+                return;
+            }
+            if (order.OrderStatus != OrderStatus.Nowe)
+            {
+                Console.WriteLine($"Zamówienie o ID {orderId} nie jest w statusie Nowe.");
+                return;
+            }
+
+            if (order.Price >= 2500 && order.PaymentMethod == PaymentMethod.GotówkaPrzyOdbiorze)
+            {
+                Task.Delay(5000);
+
+                order.OrderStatus = OrderStatus.ZwróconoDoKlienta;
+                Console.WriteLine($"ALERT: Zamówienie ID:{orderId} zostało zwrócone do klienta.");
+            }
+            else
+            {
+                Task.Delay(5000);
+
+                order.OrderStatus = OrderStatus.WMagazynie;
+                Console.WriteLine($"ALERT: Zamówienie ID:{orderId} przekazane do Magazynu.");
+            }
+        }
+        public void SendToShipping(int orderId)
+        {
+            var order = _repository.GetOrderById(orderId);
+            if (order == null)
+            {
+                Console.WriteLine($"Zamówienie o ID {orderId} nie istnieje.");
+                return;
+            }
+            if (order.OrderStatus != OrderStatus.WMagazynie)
+            {
+                Console.WriteLine($"Zamówienie o ID {orderId} nie jest w statusie W Magazynie.");
+                return;
+            }
+
+            Task.Delay(5000);
+            order.OrderStatus = OrderStatus.WWysyłce;
+            Console.WriteLine($"ALERT: Zamówienie ID: {orderId} przekazane do Wysyłki");
+
+            Task.Delay(5000);
+            order.OrderStatus = OrderStatus.Zamknięte;
+            Console.WriteLine($"ALERT: Zamówienie ID: {orderId} dostarczone");
+        }
+
+
+        public void ViewAllOrders()
+        {
+            var all = _repository.GetAllOrders();
+            foreach (var o in all)
+            {
+                Console.WriteLine($"ID: {o.Id}, {o.ProductName}, {o.OrderStatus}, Cena: {o.Price} PLN");
+            }
+        }
+        
     }
 }
---
+++ OrderProcessingApp/Repositories/InMemoryOrderRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderProcessingApp.Domain.Entities;
+
+namespace OrderProcessingApp.Repositories
+{
+    public class InMemoryOrderRepository
+    {
+        private readonly List<Order> _orders = new List<Order>();
+        public IReadOnlyList<Order> Orders => _orders;
+
+        public void AddOrder(Order order)
+        {
+
+            order.Id = _orders.Count + 1;
+            _orders.Add(order);
+        }
+
+        public void RemoveOrderById(int id)
+        {
+            var order = GetOrderById(id);
+            if (order != null)
+            {
+                _orders.Remove(order);
+
+
+                for (int i = 0; i < _orders.Count; i++)
+                {
+                    _orders[i].Id = i + 1;
+                }
+            }
+        }
+
+        public Order GetOrderById(int id)
+        {
+            if (Orders.ElementAtOrDefault(id - 1) == null)
+            {
+                return null;
+            }
+            return _orders.FirstOrDefault(o => o.Id == id);
+        }
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return _orders;
+        }
+    }
+}
