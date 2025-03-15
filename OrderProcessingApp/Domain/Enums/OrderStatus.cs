4376d0a2fdb495b6de22383962ed34d2d8ba57da
AdamN3wTS <adamjanus2004@gmail.com>
2025-03-15 11:05:06 +0100

    Dodano strukturę folderów oraz plik Enums tzn. CustomerType, OrderStatus, PaymentMethod

---
+++ OrderProcessingApp/Domain/Enums/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProcessingApp.Domain.Enums
+{
+    public enum OrderStatus
+    {
+        Nowe,
+        WMagazynie,
+        WWysyłce,
+        ZwróconoDoKlienta,
+        Błąd,
+        Zamknięte
+    }
+}
